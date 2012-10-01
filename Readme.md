8185a441cea8804d1876d59d743dd15dcb957fd5
Manuel Stofer <manuelstofer@gmail.com>
2012-10-01 23:42:41 +0200

    Initial import

---
+++ Readme.md
@@ -0,0 +1,6 @@
+
+# extend
+
+  [Underscore's extend](http://underscorejs.org/#extend) function packed as a [component](https://github.com/component/component)
+
+
---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+var each = require('each'),
+    slice = [].slice;
+
+// Extend a given object with all the properties in passed-in object(s).
+module.exports = function (obj) {
+    each(slice.call(arguments, 1), function (source) {
+        for (var prop in source) {
+            obj[prop] = source[prop];
+        }
+    });
+    return obj;
+};
